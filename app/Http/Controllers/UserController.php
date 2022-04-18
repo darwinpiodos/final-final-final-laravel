df1845dfd11c679aaeda2857c46cca4dbd9cc526
darwinpiodos <darwindivinopiodos21@gmail.com>
2022-04-18 16:20:02 +0800

    Initial Setup again kay gautro kog balik

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserController extends Controller
+{
+    function register(Request $req)
+    {
+        $user= new User;
+        $user->firstname=$req->input('firstname');
+        $user->middlename=$req->input('middlename');
+        $user->lastname=$req->input('lastname');
+        $user->email=$req->input('email');
+        $user->password=Hash::make($req->input('password'));
+
+        $user->gender=$req->input('gender');
+        $user->birthmonth=$req->input('birthmonth');
+        $user->birthday=$req->input('birthday');
+        $user->birthyear=$req->input('birthyear');
+        
+        $user->phone=$req->input('phone');
+        $user->address=$req->input('address'); 
+
+        $user->image=$req->file('image')->store('usersimage');
+
+        $user->save();
+
+        return response() -> json([
+            'status'=>200,
+            'message'=>'User Added Successfully',
+        ]);
+    }
+
+
+
+
+    
+}
