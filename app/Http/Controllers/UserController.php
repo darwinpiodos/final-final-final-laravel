73dc357cba3a0b4d7dad069e1374cc9416eb13a9
darwinpiodos <darwindivinopiodos21@gmail.com>
2022-04-19 16:20:16 +0800

    Naka Edit Update Nako

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -38,13 +38,97 @@ class UserController extends Controller
 
     function login(Request $req)
     {
-        $user= User::where('email', $req->email)->first();
-        if(!$user || !Hash::check($req->password, $user->password))
+        $user= User::where('email',$req->email)->first();
+        if(!$user || !Hash::check($req->password,$user->password))
         {
-            return ["error"=>"Email or password is not matched!"];
+            return response()->json(["error"=>"Email or password is not matched"],400);;
         }
-        return $user;
+        return response()->json(["user"=> $user],200);;
+    } 
+
+
+    
+    function list()
+    {
+
+        return User::all();
     }
+
+
+    function delete($id)
+    {
+        $result = User::where('id', $id)->delete();
+        if ($result)
+        {
+            return ["result " => "product has been deleted"];
+        }
+        else{
+            return ["result " => "Operation Failed"];
+        } 
+    }
+
+
+
+    function edit($id)
+    {
+        $user = User::find($id);
+
+        return response()-> json([
+            'status'=>200,
+            'student'=>$user
+        ]);
+    }
+
+
+
+
+        function profile($id)
+        {
+            $user = User::find($id);
+
+            return response()-> json([
+                'status'=>200,
+                'student'=>$user
+            ]);
+        }
+
+
+
+
+        
+
+        function update(Request $req, $id)
+        {
+ 
+        
+            $user= User::find($id);
+            if($user){
+            $user->firstname=$req->input('firstname');
+            $user->lastname=$req->input('lastname');
+            $user->middlename=$req->input('middlename');
+            $user->email=$req->input('email');
+            $user->password=Hash::make($req->input('password'));
+            $user->gender=$req->input('gender');
+            $user->birthmonth=$req->input('birthmonth');
+            $user->birthday=$req->input('birthday');
+            $user->birthyear=$req->input('birthyear');
+            $user->phone=$req->input('phone');
+            $user->address=$req->input('address');
+            $user->update();
+        
+                
+                return response()->json(
+                    [
+                        'status'=> 200,
+                        'message' => "Profile Updated Successfully",
+                    ]
+                    );
+                    
+                }
+                
+         
+}
+
 
 
 
--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,15 @@ use App\Http\Controllers\UserController;
 Route::post('register',[UserController::class,'register']); 
 Route::post('login',[UserController::class,'login']);
 
+Route::get('list',[UserController::class,'list']); 
+Route::delete('delete/{id}',[UserController::class,'delete']); 
+Route::get('edit/{id}',[UserController::class,'edit']); 
+Route::get('profile/{id}',[UserController::class,'profile']); 
+Route::put('update/{id}',[UserController::class,'update']); 
+ 
+
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
